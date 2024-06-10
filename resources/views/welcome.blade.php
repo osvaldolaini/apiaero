30d43435a59e5160b9032a64f040a17954221331
Osvaldo Laini <osvaldolaini@hotmail.com>
2024-06-09 21:17:06 -0300

    master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,20 +21,23 @@
     @livewireStyles
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+{{-- <body class="font-sans antialiased"> --}}
+    <body class="antialiased relative h-screen overflow-hidden bg-gray-400
+    text-white bg-cover"
+        style="background-image: url({{ url('storage/logos/error-background-20-white.png') }});">
+    <div class="min-h-screen  dark:bg-gray-900">
         {{-- @livewire('navigation-menu') --}}
 
         <!-- Page Content -->
-        <main class="relative h-screen overflow-hidden bg-white dark:bg-gray-800">
-            <header class="z-30 flex items-center w-full h-24 sm:h-32">
+        <main class="relative h-screen overflow-hidden  dark:bg-gray-800">
+            <header class="z-30 flex items-center w-full h-24 sm:h-24">
                 <div class="container flex items-center justify-between px-6 mx-auto">
-                    <div class="text-3xl font-black text-gray-800 uppercase dark:text-white">
+                    <div class="text-3xl font-black text-gray-800 uppercase ">
                         {{ env('APP_NAME') }}
                     </div>
                     <div class="flex items-center">
                         <nav
-                            class="items-center hidden text-lg text-gray-800 uppercase font-sen dark:text-white lg:flex">
+                            class="items-center hidden text-lg  uppercase font-sen  lg:flex">
                             <a href="#" class="flex px-6 py-2">
                                 Sobre
                             </a>
@@ -43,45 +46,45 @@
                             </a>
                         </nav>
                         <button class="flex flex-col ml-4 lg:hidden">
-                            <span class="w-6 h-1 mb-1 bg-gray-800 dark:bg-white">
+                            <span class="w-6 h-1 mb-1 bg-gray-800 ">
                             </span>
-                            <span class="w-6 h-1 mb-1 bg-gray-800 dark:bg-white">
+                            <span class="w-6 h-1 mb-1 bg-gray-800 ">
                             </span>
-                            <span class="w-6 h-1 mb-1 bg-gray-800 dark:bg-white">
+                            <span class="w-6 h-1 mb-1 bg-gray-800 ">
                             </span>
                         </button>
                     </div>
                 </div>
             </header>
-            <div class="relative z-20 flex items-center overflow-hidden bg-white dark:bg-gray-800">
-                <div class="container relative flex px-6 py-16 mx-auto">
+            <div class="relative z-20 flex items-center overflow-hidden  dark:bg-gray-800">
+                <div class="container relative flex px-6 pt-10 pb-16 mx-auto">
                     <div class="relative z-20 flex flex-col sm:w-2/3 lg:w-2/5">
-                        <span class="w-20 h-2 mb-12 bg-gray-800 dark:bg-white">
+                        <span class="w-20 h-2 mb-12 bg-blue-800 ">
                         </span>
                         <h1
-                            class="flex flex-col text-6xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl dark:text-white">
-                            Flight
-                            <span class="text-5xl sm:text-7xl">
-                                Time
+                            class="flex flex-col text-7xl font-black leading-none text-gray-800 uppercase font-bebas-neue sm:text-8xl ">
+                            API
+                            <span class="text-6xl sm:text-7xl text-blue-800">
+                                Aero
                             </span>
                         </h1>
-                        <p class="text-sm text-gray-700 sm:text-base dark:text-white">
-                            API.AERO é uma ferramenta destinada a facilitar a vida de quem está desenvolvendo aplicações
+                        <p class="text-lg text-black sm:text-base ">
+                            Ferramenta destinada a facilitar a vida de quem está desenvolvendo aplicações
                             para o setor aéreo.
                         </p>
                         <div class="flex mt-8">
                             <a href="{{ route('login') }}"
-                                class="px-4 py-2 mr-4 text-white uppercase bg-pink-500 border-2 border-transparent rounded-lg text-md hover:bg-pink-400">
+                                class="px-4 py-2 mr-4 text-white uppercase bg-blue-800 border-2 border-transparent rounded-lg text-md hover:bg-blue-400">
                                 Entre
                             </a>
                             <a href="{{ route('register') }}"
-                                class="px-4 py-2 text-pink-500 uppercase bg-transparent border-2 border-pink-500 rounded-lg dark:text-white hover:bg-pink-500 hover:text-white text-md">
+                                class="px-4 py-2 text-blue-800 uppercase bg-transparent border-2 border-blue-800 rounded-lg  hover:bg-blue-800 hover:text-white text-md">
                                 Registre-se
                             </a>
                         </div>
                     </div>
                     <div class="relative hidden sm:block sm:w-1/3 lg:w-3/5">
-                        <img src="{{ url('storage/logos/logo-light.png') }}" class="max-w-xs m-auto md:max-w-sm" />
+                        <img src="{{ url('storage/logos/logo-apiaero.png') }}" class="max-w-xs m-auto md:max-w-sm" />
                     </div>
                 </div>
             </div>
